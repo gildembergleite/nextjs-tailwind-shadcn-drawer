9be8ca08e34e8ae6f6198df22fadcf5ddd9410f5
Gildemberg Leite <gildembergleite@gmail.com>
2024-02-02 10:21:51 -0300

    feat: create HomePage with Drawers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
+import { DrawerDemo } from '@/components/drawer-demo'
+
 export default function Home() {
   return (
-    <main>
-      <section>
-        <h1>HOME</h1>
+    <main className="flex h-screen w-full items-center justify-center">
+      <section className="flex w-full flex-col items-center justify-center gap-12 text-center">
+        <h1 className="text-4xl font-bold">Using Drawer as a mobile menu</h1>
+        <div className="space-x-6">
+          <DrawerDemo direction="top" />
+          <DrawerDemo direction="bottom" />
+          <DrawerDemo direction="right" />
+          <DrawerDemo direction="left" />
+        </div>
       </section>
     </main>
   )
