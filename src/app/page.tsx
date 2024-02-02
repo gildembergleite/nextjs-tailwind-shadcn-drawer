3dd85afeaace386e1aac505802d904b6b94c9940
Gildemberg Leite <gildembergleite@gmail.com>
2024-02-02 10:46:15 -0300

    feat: add documentation links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 import { DrawerDemo } from '@/components/drawer-demo'
 
 export default function Home() {
@@ -10,6 +12,35 @@ export default function Home() {
           <DrawerDemo direction="bottom" />
           <DrawerDemo direction="right" />
           <DrawerDemo direction="left" />
+        </div>
+        <div className="flex flex-col gap-4">
+          <div>
+            Link to{' '}
+            <Link
+              className="underline-offset-4 hover:underline"
+              href={'https://ui.shadcn.com/'}
+              target="_blank"
+            >
+              shadcn/ui
+            </Link>
+          </div>
+          <Link
+            className="underline-offset-4 hover:underline"
+            href={'https://ui.shadcn.com/docs/components/drawer'}
+            target="_blank"
+          >
+            Drawer Documentation
+          </Link>
+          <div>
+            Drawer is built on top of{' '}
+            <Link className="underline-offset-4 hover:underline" href={''}>
+              Vaul
+            </Link>{' '}
+            by{' '}
+            <Link className="underline-offset-4 hover:underline" href={''}>
+              emilkowalski_.
+            </Link>
+          </div>
         </div>
       </section>
     </main>
