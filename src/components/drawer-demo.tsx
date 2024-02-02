19adf1fbba3329b2b4394923588081a419f951d8
Gildemberg Leite <gildembergleite@gmail.com>
2024-02-02 10:21:29 -0300

    feat: created DrawerDemo component

---
+++ src/components/drawer-demo.tsx
@@ -0,0 +1,42 @@
+import { Button } from '@/components/ui/button'
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger
+} from '@/components/ui/drawer'
+
+interface DrawerDemoProps {
+  direction: 'left' | 'right' | 'top' | 'bottom'
+}
+
+export function DrawerDemo({ direction }: DrawerDemoProps) {
+  return (
+    <Drawer direction={direction}>
+      <DrawerTrigger asChild>
+        <Button variant="outline" className="capitalize">
+          {direction}
+        </Button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <div className="mx-auto w-full max-w-sm">
+          <DrawerHeader>
+            <DrawerTitle>Move Goal</DrawerTitle>
+            <DrawerDescription>Set your daily activity goal.</DrawerDescription>
+          </DrawerHeader>
+
+          <DrawerFooter>
+            <Button>Submit</Button>
+            <DrawerClose asChild>
+              <Button variant="outline">Cancel</Button>
+            </DrawerClose>
+          </DrawerFooter>
+        </div>
+      </DrawerContent>
+    </Drawer>
+  )
+}
